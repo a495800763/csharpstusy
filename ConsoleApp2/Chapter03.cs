786524e5fb4b86e9d9663bee9f6c4812709943a9
liumq <495800763@qq.com>
2021-07-11 14:00:55 +0800

    asdas

---
+++ ConsoleApp2/Chapter03.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Chapter03
+    {
+        private static void Main(string[] args)
+        {
+            double firstNumber, secondNumber;
+            String userName;
+            Console.WriteLine("Enter yiur name");
+            userName = Console.ReadLine();
+            Console.WriteLine($"welcome,{userName}");
+            Console.WriteLine("now give me a numBer");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("now give me another numBer");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"thje sum of {firstNumber} and {secondNumber} is {firstNumber + secondNumber}");
+            Console.WriteLine($"thje result of substrcing {secondNumber} from  {firstNumber} is {firstNumber - secondNumber}");
+            Console.WriteLine($"thje product of {firstNumber} and {secondNumber} is {firstNumber * secondNumber}");
+            Console.WriteLine($"thje result of dividing {firstNumber} by {secondNumber} is {firstNumber / secondNumber}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    int myInteger;
+        //    string myString;
+        //    myInteger = 17;
+        //    myString="\" myInteger \" is/8* ";
+        //    Console.WriteLine($"{myString} {myInteger}");
+        //    Console.ReadKey();
+        //}
+    }
+}
