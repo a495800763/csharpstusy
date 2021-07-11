452191c36e56f213b51fad2eb348389b188442af
liumq <495800763@qq.com>
2021-07-11 23:35:22 +0800

    bool值

---
+++ ConsoleApp2/BooleanTest.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using static System.Convert;
+
+namespace ConsoleApp2
+{
+    internal class BooleanTest
+    {
+        private static void Main(string[] args)
+        {
+            WriteLine("Enter an Integer");
+            int myInt = ToInt32(ReadLine());
+            bool isLessThan10 = myInt < 10;
+            bool isBetween0And5 = (myInt >= 0) && (myInt <= 5);
+            WriteLine($"Integer less than 10 ? {isLessThan10}");
+            WriteLine($"Integer between 0 amnd 5 ? {isBetween0And5}");
+            WriteLine($"Exactly one of above is true : " +
+                $"{isLessThan10 ^ isBetween0And5}");
+        }
+    }
+}
