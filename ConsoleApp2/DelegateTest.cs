9078bff455e3645a185a5ead3deef0eaedad3a63
liumq <495800763@qq.com>
2021-09-18 22:46:57 +0800

    delegate

---
+++ ConsoleApp2/DelegateTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// 委托测试
+    /// </summary>
+    class DelegateTest
+    {
+        delegate double ProcessDelegate(double param1, double param2);
+        public static double Multiply(double a,double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public static void Function()
+        {
+            ProcessDelegate process;
+            Console.WriteLine("Enter 2 number swparated with comma :");
+            string input = Console.ReadLine();
+            string[] split = input.Split(',');
+            double p1 = Convert.ToDouble(split[0]);
+            double p2 = Convert.ToDouble(split[1]);
+            Console.WriteLine("Enter M  4 Multiply and D  4 Devided :");
+            string option = Console.ReadLine();
+            if (option == "M")
+            {
+                process = new ProcessDelegate(Multiply);
+            }else if (option == "D")
+            {
+                process = new ProcessDelegate(Divide);
+            }
+            else
+            {
+                process = null;
+                Console.WriteLine("请输入正确的操作符");
+
+            }
+            Console.WriteLine($"Result:{ process(p1,p2)}");
+
+        } 
+    }
+}
---
+++ ConsoleApp2/StructFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// 结构体中的函数 测试
+    /// </summary>
+    class StructFunction
+    {
+        public static void Function (string[] args)
+        {
+            CustomerName person;
+            person.firstName = "Liu";
+            person.lastName = "Mengqi";
+            Console.WriteLine(person.Name());
+        }
+    }
+
+    struct CustomerName
+    {
+        public string firstName, lastName;
+        //结构体中定义函数
+        public string Name() => firstName + ";" + lastName;
+    }
+}
