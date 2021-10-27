35468475e483f8bbc50fb7d8f257154b9f6cbf94
liumq <495800763@qq.com>
2021-10-27 22:26:03 +0800

    Collections

---
+++ ConsoleApp2/Collections/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Collections
+{
+    public abstract class Animal
+    {
+        protected string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Animal() => name = "The Animal with no name";
+
+        public Animal(string name) => this.name = name;
+
+        public void feed() => Console.WriteLine($"{name} has been fed.");
+
+    }
+}
---
+++ ConsoleApp2/Collections/Cow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Collections
+{
+    public class Cow : Animal
+    {
+        public void Milk() => Console.WriteLine($"{name} has been milked.");
+        public Cow(string name) : base(name) { }
+    }
+}
