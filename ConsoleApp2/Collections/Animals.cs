691dd8e1d52af78a3f8a74463190b90249092d88
liumq <495800763@qq.com>
2021-10-27 23:07:09 +0800

    CollectionBase

---
+++ ConsoleApp2/Collections/Animals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Collections
+{
+    /// <summary>
+    /// 继承CollectionBase 类实现Animal类对象的集合类
+    /// </summary>
+    public class Animals : CollectionBase 
+    {
+        public void Add(Animal newAnimal)
+        {
+            List.Add(newAnimal);
+        }
+
+        public void  Remove(Animal newAnimal)
+        {
+            List.Remove(newAnimal);
+        }
+
+        //定义索引符
+        public Animal this[int animalIndex]
+        {
+            get
+            {
+                return (Animal)List[animalIndex];
+            }
+            set
+            {
+                List[animalIndex] = value;
+            }
+        }
+    }
+}
