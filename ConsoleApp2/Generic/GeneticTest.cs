00ba9046b7fe8e8bf3d1d675ee953a8cfa59fc5c
liumq <495800763@qq.com>
2021-11-15 23:06:58 +0800

    泛型类型

---
+++ ConsoleApp2/Generic/GeneticTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Generic
+{
+    class GeneticTest<T>
+    {
+        public bool Compare(T op1,T op2)
+        {
+            //if (op1 == op2)
+            //{
+            //    //如果不对T类型做出where 限制，则可能的类型不支持==操作符
+            //}
+
+            return false;
+        }
+    }
+}
