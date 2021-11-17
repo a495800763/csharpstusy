edf60789d61bad2f65d368a8b0eade627f665cd4
liumq <495800763@qq.com>
2021-11-17 22:31:39 +0800

    CARDLIB DEMO

---
+++ CardLib/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardLib
+{
+    public class Card
+    {
+        public Rank rank;
+        public Suit suit;
+
+        private Card()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Card(Suit newSuit, Rank newRank)
+        {
+            suit = newSuit;
+            rank = newRank;
+        }
+
+        public override string ToString()
+        {
+            return "The" + rank + "of" + suit + ".";
+        }
+    }
+}
